92c2b23a7080482dd91da3f68cca1708c4bf873c
Kushal Babel <babel97kushal@gmail.com>
2022-03-09 18:05:23 -0500

    generate agg problems

--- generate_client_problems.sh
+++ generate_client_problems.sh
@@ -5,7 +5,7 @@ curr_block=$start_block
 while [ $curr_block -le $end_block ]
 do
     curr_end_block=$(( $curr_block + 25 ))
-    cmd="python3 generate_client_problems.py -f ../mev/data-scripts/latest-data/sushiswap-processed/0x397ff1542f962076d0bfe58ea045ffa2d347aca0.csv -sb $curr_block -eb $curr_end_block -o clienttests_sushi"
+    cmd="python3 generate_agg_client_problems.py -f ../mev/data-scripts/latest-data/sushiswap-processed/0x397ff1542f962076d0bfe58ea045ffa2d347aca0.csv -sb $curr_block -eb $curr_end_block -o clienttests_agg"
     echo $cmd
     eval $cmd
     curr_block=$curr_end_block
