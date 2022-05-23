10d83fc96a2acf439eefc7504506cc190e36c20d
Kushal Babel <babel97kushal@gmail.com>
2022-05-23 18:10:51 -0400

    minor:

--- ping_anvils.sh
+++ ping_anvils.sh
@@ -4,4 +4,5 @@ for ((i=0;i<CORES;i++)); do
 	PORT=$(( 8521 + $i ))
 	echo $PORT
 	curl -X POST localhost:$PORT --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'  --header 'Content-Type: application/json'
+	echo ""
 done
